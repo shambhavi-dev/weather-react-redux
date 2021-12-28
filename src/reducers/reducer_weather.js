88ac8168c5c387d67f6820c8214f7fb52a26320c
shambhavi-dev <73819465+shambhavi-dev@users.noreply.github.com>
2021-12-28 15:39:03 +0530

    Update reducer_weather.js

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,8 +2,8 @@ import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state=[], action) {
   console.log('Action Received ', action);
-  switch (action.type) {
-    case FETCH_WEATHER:
+  if (action.type==FETCH_WEATHER) {
+   // case FETCH_WEATHER:
       // return state.concat( [ action.payload.data ] );
       return [action.payload.data, ...state]
   }
